4958725dd586adeceae5543697cf19bbf97df33d
Hall <1234@mail.com>
2025-03-16 15:41:26 +0800

    Remove unnecessary comments and clean up merge sort implementation in Swift

--- Swift/排序/归并.swift
+++ Swift/排序/归并.swift
@@ -1,7 +1,4 @@
 var numbers = [2,6,3,5,1,4]
-//write algorithm by my self, do not auto-complete!
-let size = numbers.count
-
 func merge(_ left: [Int], _ right: [Int]) -> [Int] {
     var result: [Int] = []
     var leftIndex: Int = 0    // 左数组的当前位置
